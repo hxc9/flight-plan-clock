5840d91e4205f57a57b0600dd9e5e2593c5b3095
Alexis Hafner <alexis.hafner@convertic.com>
2023-01-10 16:32:47 +0100

    GUI proto

--- apps/flight-plan-clock-backend/components/flightPlans/ctot.tsx
+++ apps/flight-plan-clock-backend/components/flightPlans/ctot.tsx
@@ -1,24 +1,17 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc"
 import {ZuluTimestamp} from "../time/zuluTimestamp";
-import {getFplCtot} from "../../lib/ctotService";
+import {getFplCtot, parseCtot} from "../../lib/ctotService";
 
 dayjs.extend(utc)
 
 export const Ctot = async ({fplId, eobt: eobtTimestamp} : {fplId: number, eobt: number}) : Promise<JSX.Element> =>  {
     const ctotString = await getFplCtot(fplId)
 
-    if (ctotString) {
-        const parsedCtot = /^(\d\d):?(\d\d)$/.exec(ctotString)
-        if (parsedCtot) {
-            const [, hours, minutes] = parsedCtot
-            const eobt = dayjs.unix(eobtTimestamp).utc()
-            let ctot = eobt.hour(+hours).minute(+minutes)
-            if (ctot.isBefore(eobt)) {
-                ctot = ctot.add(1, 'day')
-            }
-            return <ZuluTimestamp unixTimestamp={ctot.unix()} withDate={false} withDelta={true}/>
-        }
+    const ctot = parseCtot(ctotString, eobtTimestamp)
+
+    if (ctot) {
+        return <ZuluTimestamp unixTimestamp={ctot.unix()} withDate={false} withDelta={true}/>
     }
 
     return <>{ctotString}</>
--- apps/flight-plan-clock-backend/lib/ctotService.ts
+++ apps/flight-plan-clock-backend/lib/ctotService.ts
@@ -1,7 +1,8 @@
 import {ChainableCommander} from "ioredis";
 import {defaultExpiry, fplKey, redis} from "./dbClient";
+import dayjs, {Dayjs} from "dayjs";
 
-export async function getFplCtot(fplId: number) : Promise<string|null> {
+export async function getFplCtot(fplId: number): Promise<string | null> {
     return redis.get(ctotKey(fplId))
 }
 
@@ -17,4 +18,25 @@ export function deleteFplCtot(pipeline: ChainableCommander, fplId: number) {
 
 function ctotKey(fplId: number) {
     return `${fplKey(fplId)}:ctot`;
+}
+
+export function parseCtot(ctotString: string | null, eobt: number | Dayjs): Dayjs | null {
+    if (!ctotString) {
+        return null
+    }
+
+    const parsedCtot = /^(\d\d):?(\d\d)$/.exec(ctotString)
+    if (!parsedCtot) {
+        return null
+    }
+    const [, hours, minutes] = parsedCtot
+
+    if (typeof eobt === 'number') {
+        eobt = dayjs.unix(eobt).utc()
+    }
+    let ctot = eobt.hour(+hours).minute(+minutes)
+    if (ctot.isBefore(eobt)) {
+        ctot = ctot.add(1, 'day')
+    }
+    return ctot
 }
