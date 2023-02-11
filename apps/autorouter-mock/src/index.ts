d85519e4f4884820f04521d59d0f1a7c3a9e7c62
Alexis Hafner <alexis.hafner@gmail.com>
2023-02-11 23:56:59 +0100

    Revert "Vercel/TurboRepo support fixes"
    This reverts commit 3fa396c2a69ad4bf625f431931d2535e6a8cd2ec.

--- apps/autorouter-mock/src/index.ts
+++ apps/autorouter-mock/src/index.ts
@@ -6,7 +6,6 @@ import message from './routes/message';
 dotenv.config({path: `.env.${process.env.NODE_ENV || 'local'}`})
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Autorouter mock server');
@@ -17,6 +16,4 @@ app.use(express.json());
 app.use('/api/flightPlan', flightPlan);
 app.use('/api/message', message);
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+export default app
