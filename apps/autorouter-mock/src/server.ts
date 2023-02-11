3fa396c2a69ad4bf625f431931d2535e6a8cd2ec
Alexis Hafner <alexis.hafner@gmail.com>
2023-02-11 23:55:03 +0100

    Vercel/TurboRepo support fixes

--- apps/autorouter-mock/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './index'
-
-
-const port = process.env.MOCK_PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
